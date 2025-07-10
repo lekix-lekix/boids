50ffab3ae27ec52c4cb42b823cc735e61a5d2e1b
lekix-lekix <pouliquen.k@gmail.com>
2025-07-11 01:13:11 +0200

    triangle wip

--- src/main.ts
+++ src/main.ts
@@ -28,49 +28,84 @@ ctx.beginPath();
 ctx.arc(cx, cy, R, 0, 2 * Math.PI);
 ctx.stroke();
 
-const getTriangle = (x, y, R) => {
-    const triangle = {
-        //the first vertex is on the circumscribed circle at 0 radians where R is the radius of the circle ( R)
-        //you may decide to change this.
-        x1: x + R,
-        y1: y,
-        //the second vertex is on the circumscribed circle at 2*Math.PI/3 radians 
-        //you may decide to change this.
-        x2: x + R * Math.cos(2*Math.PI/3),
-        y2: y + R * Math.sin(2*Math.PI/3),
-        //calculate the 3-rd vertex
-        x3: x + R * Math.cos(4*Math.PI/3),
-        y3: y + R * Math.sin(4*Math.PI/3)
-    };
-    return triangle;
-}
+class Triangle {
+    public x: number;
+    public y: number;
+    public R: number;
+    public v1: {x: number, y: number};
+    public v2: {x: number, y: number};
+    public v3: {x: number, y: number};
+    private angle: number = 0;
 
+    constructor(xArg: number, yArg:number, RArg:number) {
+        this.x = xArg;
+        this.y = yArg;
+        this.R = RArg;
+
+        this.v1 = {x: 0, y: 0};
+        this.v2 = {x: 0, y: 0};
+        this.v3 = {x: 0, y: 0};
+        this.setVertices();
+    }
+
+    public setVertices() {
+        this.v1.x = this.x + this.R * Math.cos(this.angle);
+        this.v1.y = this.y + this.R * Math.sin(this.angle);
+        this.v2.x = this.x + this.R * Math.cos(2 * Math.PI / 3 + this.angle);
+        this.v2.y = this.y + this.R * Math.sin(2 * Math.PI / 3 + this.angle);
+        this.v3.x = this.x + this.R * Math.cos(4 * Math.PI / 3 + this.angle);
+        this.v3.y = this.y + this.R * Math.sin(4 * Math.PI / 3 + this.angle);        
+    }
+
+    public draw() {
+        ctx.beginPath();
+        ctx.moveTo(this.v1.x, this.v1.y);
+        ctx.lineTo(this.v2.x, this.v2.y);
+        ctx.lineTo(this.v3.x, this.v3.y);
+        ctx.lineTo(this.v1.x, this.v1.y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    public update(newAngle: number) {
+        this.angle = (this.angle + newAngle) % (2 * Math.PI);
+        this.setVertices();
+    }
+    
+    public updateDegrees(newAngleDegrees: number) {
+        const newAngleRadians = (newAngleDegrees * Math.PI) / 180;
+        this.update(newAngleRadians);
+    }
+}
 
 const clearScreen = () => {
     ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
 };
 
 ctx.strokeStyle = "red";
-
-const drawTriangle = (triangle) => {
-    ctx.beginPath();
-    ctx.moveTo(triangle.x1, triangle.y1);
-    ctx.lineTo(triangle.x2, triangle.y2);
-    ctx.lineTo(triangle.x3, triangle.y3);
-    ctx.lineTo(triangle.x1, triangle.y1);
-    ctx.closePath();
-    ctx.stroke();
-}
-
-let myTriangle = getTriangle(cx, cy, R);
+let myTriangle = new Triangle(cx, cy, R);
+let myTriangle2 = new Triangle(cx - 10, cy - 10, R);
+let myTriangle3 = new Triangle(cx - 20, cy - 20, R);
+let myTriangle4 = new Triangle(cx - 30, cy - 30, R);
 
 const launchAnim = () => {
-        clearScreen();
-		ctx.fillStyle = "rgb(255, 0, 0)";
-        myTriangle = getTriangle(cx, cy, R);
-        drawTriangle(myTriangle);
-        cx++;
-        requestAnimationFrame(launchAnim);
+    clearScreen();
+    ctx.fillStyle = "rgb(255, 0, 0)";
+    myTriangle.draw();
+    myTriangle2.draw();
+    myTriangle3.draw();
+    myTriangle4.draw();
+    // myTriangle.update(1);
+    // if (myTriangle.v1.x > ctx.canvas.width && myTriangle.v2.x > ctx.canvas.width && myTriangle.v3.x > ctx.canvas.width)
+    // {
+        myTriangle.updateDegrees(1);
+        myTriangle2.updateDegrees(1);
+        myTriangle3.updateDegrees(1);
+        myTriangle4.updateDegrees(1);
+        // cx = 0;
+        // myTriangle.setVertices();
+    // }
+    requestAnimationFrame(launchAnim);
 }
 
 requestAnimationFrame(launchAnim);

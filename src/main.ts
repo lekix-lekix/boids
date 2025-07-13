6b723e5f4127e32db817be9c135160e3b9509ebd
lekix-lekix <pouliquen.k@gmail.com>
2025-07-13 02:42:52 +0200

    target, move, rotate

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <canvas id="canvas"></canvas>
 `
 var canvasElement = document.querySelector("#canvas");
-var ctx = canvasElement.getContext("2d");
+var ctx = canvasElement!.getContext("2d");
 // the width of the canvas
 let cw = (canvasElement.width = 1000),
   cx = cw / 2;
@@ -16,10 +16,7 @@ let ch = (canvasElement.height = 1000),
   cy = ch / 2;
   //your data
 
-let L = 60
-let a = 20,
-  b = 20,
-  c = L;
+let L = 50;
 
 let R = (L *.5) / Math.cos(Math.PI/6);
 
@@ -35,16 +32,21 @@ class Triangle {
     public v1: {x: number, y: number};
     public v2: {x: number, y: number};
     public v3: {x: number, y: number};
+    public center: {x: number, y: number};
+    public target: {x: number, y: number};
     private angle: number = 0;
+    private maxTurn: number = 0.05;
+    public targetAngle: number = 0;
 
     constructor(xArg: number, yArg:number, RArg:number) {
         this.x = xArg;
         this.y = yArg;
         this.R = RArg;
-
         this.v1 = {x: 0, y: 0};
         this.v2 = {x: 0, y: 0};
         this.v3 = {x: 0, y: 0};
+        this.center = {x: 0, y: 0};
+        this.target = {x: 0, y: 0};
         this.setVertices();
     }
 
@@ -57,6 +59,15 @@ class Triangle {
         this.v3.y = this.y + this.R * Math.sin(4 * Math.PI / 3 + this.angle);        
     }
 
+    public getVerticeLength(vertice1: {x: number, y:number}, vertice2: {x: number, y:number}) {
+        return (Math.sqrt(vertice2.x - vertice1.x) ** 2 + (vertice2.y - vertice1.y) ** 2);
+    }
+
+    public setCenter() {
+        this.center.x = (this.v1.x + this.v2.x + this.v3.x ) / 3;
+        this.center.y = (this.v1.y + this.v2.y + this.v3.y ) / 3;
+    }
+
     public draw() {
         ctx.beginPath();
         ctx.moveTo(this.v1.x, this.v1.y);
@@ -65,16 +76,50 @@ class Triangle {
         ctx.lineTo(this.v1.x, this.v1.y);
         ctx.closePath();
         ctx.stroke();
+        ctx.fillStyle = "rgb(0, 255, 0)";
+        ctx.fillRect(this.center.x, this.center.y, 1, 1);
+    };
+
+    public rotateToTarget() {
+        let angleDiff = this.targetAngle - this.angle;
+        if (angleDiff > Math.PI) angleDiff -= 2 * Math.PI;
+        else if (angleDiff < Math.PI * -1) angleDiff += 2 * Math.PI;
+        if (angleDiff > this.maxTurn) this.angle += this.maxTurn;
+        else if (angleDiff < this.maxTurn * -1) this.angle -= this.maxTurn;
+        else this.angle += angleDiff;
     }
 
-    public update(newAngle: number) {
-        this.angle = (this.angle + newAngle) % (2 * Math.PI);
-        this.setVertices();
+    public moveToTarget() {
+        const xDir = Math.cos(this.angle);
+        const yDir = Math.sin(this.angle);
+        const dx = xDir * 2;
+        const dy = yDir * 2;
+        this.x += dx;
+        this.y += dy;
+        // this.v1.x += dx;
+        // this.v1.y += dy;
+        // this.v2.x += dx;
+        // this.v2.y += dy;
+        // this.v3.x += dx;
+        // this.v3.y += dy;
     }
-    
-    public updateDegrees(newAngleDegrees: number) {
-        const newAngleRadians = (newAngleDegrees * Math.PI) / 180;
-        this.update(newAngleRadians);
+
+    public setTargetAngle() {
+        const targetVec = {dx: this.target.x - this.center.x, dy: this.target.y - this.center.y}; 
+        this.targetAngle = Math.atan2(targetVec.dy, targetVec.dx)
+    }
+
+    public update() {
+        // const newAngleRadians = (this.targetAngle * Math.PI) / 180;
+        // this.angle = (this.angle + newAngleRadians) % (2 * Math.PI);
+        if (this.targetAngle != this.angle)
+            this.rotateToTarget();
+        if (this.center.x != this.target.x || this.center.y != this.target.y)
+            this.moveToTarget();
+        this.setVertices();
+        this.setCenter();
+        this.setTargetAngle();
+        console.log(this.targetAngle, this.angle);
     }
 }
 
@@ -93,27 +138,27 @@ for (let i = 0; i < 80; i++)
     cy -= 5;
 }
 
+const triangle = new Triangle(500, 500, R);
+
+document.addEventListener("click", (event) => {
+    triangle.target.x = event.x;
+    triangle.target.y = event.y;
+})
+
 const launchAnim = () => {
     clearScreen();
-    ctx.fillStyle = "rgb(255, 0, 0)";
-    let deg = 1;
-    for (const triangle of triangles)
-    {
-        triangle.draw();
-        triangle.updateDegrees(deg);
-        deg += 0.005;
-
-    }
-    // myTriangle.update(1);
-    // if (myTriangle.v1.x > ctx.canvas.width && myTriangle.v2.x > ctx.canvas.width && myTriangle.v3.x > ctx.canvas.width)
-    // // {
-    //     myTriangle.updateDegrees(1);
-    //     myTriangle2.updateDegrees(1.1);
-    //     myTriangle3.updateDegrees(1.2);
-    //     myTriangle4.updateDegrees(1.3);
-        // cx = 0;
-        // myTriangle.setVertices();
+    // let deg = 1;
+    // for (const triangle of triangles)
+    // {
+    //     ctx.fillStyle = "rgb(255, 0, 0)";
+    //     triangle.draw();
+    //     triangle.update();
+    //     // deg += 0.005;
     // }
+    console.log(triangle.target.x, triangle.target.y);
+    console.log(triangle.x, triangle.y);
+    triangle.draw();
+    triangle.update();
     requestAnimationFrame(launchAnim);
 }
 
